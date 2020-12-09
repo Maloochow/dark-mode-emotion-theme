10dfe2fb7eaf7215d049192612d735d7efa930ab
Maloochow <ml.maloo.angel@gmail.com>
2020-12-09 10:43:30 -0500

    toggle function implemented

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { ThemeProvider } from './ThemeContext'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider>
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
